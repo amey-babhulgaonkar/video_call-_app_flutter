ec09b97cc8201d07e9846e82bb44f98b61dd0e6e
Amey <amey.babhulgaonkar123@gmail.com>
2026-04-10 19:40:38 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Video Call App"),
+      ),
+      body: Center(
+        child: Text("Ready to build 🚀"),
+      ),
+    );
+  }
+}
