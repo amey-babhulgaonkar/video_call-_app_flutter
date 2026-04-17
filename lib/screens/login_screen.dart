348d7c932c7ad81111592a65460bf25c3c507de1
Amey <amey.babhulgaonkar123@gmail.com>
2026-04-17 20:15:35 +0530

    Login screen ui made today now next  task is to add firebase authentication to it

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,51 +1,86 @@
-    import 'package:flutter/material.dart';
-    import '../widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+import '../widgets/custom_button.dart';
 
-    class LoginScreen extends StatelessWidget{
-        @override
-        Widget build(BuildContext context){
-            return Scaffold(
-                body:Center(
-                    child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal:40),
-                        child:Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children:[
-                        Text("Login"),
-                        SizedBox(height:60),
-                        Text("Username"),
-                        SizedBox(height:10),
-                        TextFormField(
-                            decoration:InputDecoration(
-                                hintText:"Enter Username",
-                                border:OutlineInputBorder(
-                                    borderRadius:BorderRadius.circular(12),
-                                ),
-                            ),
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 37, 35, 35),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/logo.png", height: 80),
+              SizedBox(height: 30),
+              //box around we wrap the login in container
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  border: Border.all(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                //box designing here
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsetsGeometry.directional(
+                        start: 20,
+                        end: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 1),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        SizedBox(height:20),
-                        Text("Password"),
-                        TextFormField(
-                            decoration:InputDecoration(
-                                hintText:"Enter Password",
-                                border:OutlineInputBorder(
-                                    borderRadius:BorderRadius.circular(12),
-                                ),
-                            ),
-                            obscureText:true,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Text("Username"),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        prefixIcon: Icon(Icons.person),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                        SizedBox(height:60),
-                        CustomButton(
-                            text:"Login",
-                            onPressed:(){
-                                print("Clicked");
-                            },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text("Password"),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                    ],
-                ),)
-            ),
-            );
-        }
-    }
-
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 60),
+                    CustomButton(
+                      text: "Login",
+                      onPressed: () {
+                        print("Clicked");
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -12,8 +12,18 @@ class CustomButton extends StatelessWidget{
  @override
  Widget build(BuildContext context){
    return ElevatedButton(
+    style:ElevatedButton.styleFrom(
+      backgroundColor: const Color.fromARGB(255, 73, 185, 236),
+      foregroundColor: Colors.white,
+      padding: EdgeInsets.symmetric(vertical: 10,horizontal: 40),
+      elevation:6,
+  ),
       onPressed:onPressed,
-      child: Text(text),
+      child: Text(text,
+      style: TextStyle(
+        fontSize: 20,
+        letterSpacing: 1.2,
+      ),),
    );
  }
 }
